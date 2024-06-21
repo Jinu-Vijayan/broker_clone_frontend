f2dc64b63ec2cfa041bf8f62a210b733ed9cec78
Jinu Vijayan <jinuvijayan01@gmail.com>
2024-06-21 13:56:23 +0530

    Change: changed the vite config proxy target to point to the hosted backend

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,11 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    proxy : {
-      '/api' : {
-        target : "http://localhost:10000",
-        secure : false
+    proxy: {
+      '/api': {
+        target: 'https://broker-clone-backend.onrender.com',
+        changeOrigin: true,
+        secure: false,
       }
     }
   },
